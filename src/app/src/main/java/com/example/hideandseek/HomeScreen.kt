adb537c9e0196d55dd16c288805b68dc88f57310
viclok <110517011+viclok@users.noreply.github.com>
2023-09-20 23:37:57 +1000

    added UI activity and layouts files

---
+++ src/app/src/main/java/com/example/hideandseek/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.hideandseek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+    }
+}
---
+++ src/app/src/main/java/com/example/hideandseek/NewGameSettings.kt
@@ -0,0 +1,11 @@
+package com.example.hideandseek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewGameSettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_game_settings)
+    }
+}
