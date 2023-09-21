9ae3c194536556ab04cad046527f783737061ad4
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2023-09-21 15:36:19 +1000

    feature: Enabled the reflection of user's location on map

--- src/app/src/main/java/com/example/hideandseek/GamePlay.kt
+++ src/app/src/main/java/com/example/hideandseek/GamePlay.kt
@@ -1,23 +1,60 @@
 package com.example.hideandseek
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.hideandseek.databinding.GamePlayBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.hideandseek.databinding.GamePlayBinding
+
 
 class GamePlay : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
     private lateinit var binding: GamePlayBinding
+
+    // Google's API for location services
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+
+    // configuration of all settings of FusedLocationProviderClient
+    var locationRequest: LocationRequest? = null
+    var locationCallBack: LocationCallback? = null
+    private val Request_Code_Location = 22
+
+    // interval in milliseconds for location updates
+    private var updateInterval: Long = 10000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // location API settings
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.create()
+        locationRequest!!.interval = updateInterval
+        locationCallBack = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                if (locationResult != null) {
+                    Log.d("LocationTest", "Location updates")
+                    locationResult.lastLocation?.let { updateMap(it, map) }
+                } else {
+                    Log.d("LocationTest", "Location updates fail: null")
+                }
+            }
+        }
 
         binding = GamePlayBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -30,19 +67,75 @@ class GamePlay : AppCompatActivity(), OnMapReadyCallback {
 
     /**
      * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
+        updateLocation(map)
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    /**
+     * Obtain the last location of the hider and update the map.
+     */
+    private fun updateLocation(googleMap: GoogleMap) {
+        //if user grants permission
+        if (ActivityCompat.checkSelfPermission(
+                this@GamePlay,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient!!.requestLocationUpdates(
+                locationRequest!!,
+                locationCallBack!!,
+                null
+            )
+
+            // get the last location
+            fusedLocationClient!!.lastLocation
+                .addOnSuccessListener(
+                    this
+                ) { location ->
+                    if (location == null) {
+                        Log.d("LocationTest", "null")
+                    } else {
+                        Log.d("LocationTest", "Success")
+                        updateMap(location, googleMap) // if successful, update the UI
+                    }
+                }
+        } else {
+            //if user hasn't granted permission, ask for it explicitly
+            ActivityCompat.requestPermissions(
+                this@GamePlay,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                Request_Code_Location
+            )
+        }
+    }
+
+    /**
+     * Reflect the user's location on the map.
+     */
+    private fun updateMap(location: Location, googleMap: GoogleMap) {
+        map = googleMap
+        // extract the coordinates
+        val lat = location.latitude
+        val lon = location.longitude
+        val user = LatLng(lat, lon)
+
+        // add the marker and adjust
+        map.addMarker(MarkerOptions().position(user).title("User"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(user))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Request_Code_Location) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                updateLocation(map)
+            }
+        }
     }
 }
