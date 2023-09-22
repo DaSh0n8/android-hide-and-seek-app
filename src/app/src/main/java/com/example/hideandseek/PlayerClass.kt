9210fc3c8d2a5f3757eaeeaf385ed15b2c54e49e
Brandon <brandonveili@student.unimelb.edu.au>
2023-09-22 17:10:41 +1000

    Adding database dependencies and changing landing page for testing purposes

--- src/app/src/main/java/com/example/hideandseek/PlayerClass.kt
+++ src/app/src/main/java/com/example/hideandseek/PlayerClass.kt
@@ -1,11 +1,27 @@
 package com.example.hideandseek
 
-class PlayerClass(
-    var userName: String,
-    var seeker: Boolean,
-    var latitude: Double,
-    var longitude: Double,
-    var status: Boolean,
-    var host: Boolean
+class PlayerClass() {
+    var userName: String = ""
+    var seeker: Boolean = false
+    var latitude: Double? = 0.0
+    var longitude: Double? = 0.0
+    var status: Boolean = false
+    var host: Boolean = false
 
-)
+    constructor(
+        userName: String,
+        seeker: Boolean,
+        latitude: Double?,
+        longitude: Double?,
+        status: Boolean,
+        host: Boolean
+    ) : this() {
+        this.userName = userName
+        this.seeker = seeker
+        this.latitude = latitude
+        this.longitude = longitude
+        this.status = status
+        this.host = host
+    }
+}
+
