17deade0160501a1650bd9746562ea6dd2b8a021
Brandon <brandonveili@student.unimelb.edu.au>
2023-09-22 17:11:30 +1000

    Implemented create game function and increasing length of input for username

--- src/app/src/main/java/com/example/hideandseek/HomeScreen.kt
+++ src/app/src/main/java/com/example/hideandseek/HomeScreen.kt
@@ -1,11 +1,58 @@
 package com.example.hideandseek
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
+import java.util.Random
 
 class HomeScreen : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
+
+        val createGameButton: Button = findViewById(R.id.createGameButton)
+        createGameButton.setOnClickListener {
+            createButtonClicked()
+        }
+        val joinGameButton: Button = findViewById(R.id.joinGameButton)
+        joinGameButton.setOnClickListener {
+            joinButtonClicked()
+        }
+        FirebaseApp.initializeApp(this)
+        val databaseUrl = "https://db-demo-26f0a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        database = FirebaseDatabase.getInstance(databaseUrl)
+    }
+
+    private fun createButtonClicked(){
+        val usernameInput: EditText = findViewById(R.id.username_input)
+        val username: String = usernameInput.text.toString()
+
+        val intent = Intent(this, NewGameSettings::class.java)
+        intent.putExtra("username_key", username)
+        startActivity(intent)
+
+//        val gameSessionRef = database.getReference("gameSessions").push()
+//
+//        val players = listOf(
+//            PlayerClass(username, true, null, null, true, true)
+//        )
+//        val sessionId: Int = Random().nextInt(9999 - 1000 + 1) + 1000
+//        val sessionIdString: String = sessionId.toString()
+//        val newGameSession = GameSessionClass(sessionIdString, "ongoing", players, 300)
+//        gameSessionRef.setValue(newGameSession)
+    }
+
+    private fun joinButtonClicked(){
+        val usernameInput: EditText = findViewById(R.id.username_input)
+        val username: String = usernameInput.text.toString()
+
+        val intent = Intent(this, JoinGame::class.java)
+        intent.putExtra("username_key", username)
+        startActivity(intent)
     }
 }
--- src/app/src/main/java/com/example/hideandseek/NewGameSettings.kt
+++ src/app/src/main/java/com/example/hideandseek/NewGameSettings.kt
@@ -2,10 +2,55 @@ package com.example.hideandseek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.FirebaseDatabase
+import java.util.Random
 
 class NewGameSettings : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_game_settings)
+        val createGameButton: Button = findViewById(R.id.btnStartGame)
+
+        val receivedUsername: String? = intent.getStringExtra("username_key")
+
+        createGameButton.setOnClickListener {
+            createButtonClicked(receivedUsername)
+        }
+
+        FirebaseApp.initializeApp(this)
+        val databaseUrl = "https://db-demo-26f0a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        database = FirebaseDatabase.getInstance(databaseUrl)
+    }
+
+    private fun createButtonClicked(username: String?){
+        if (username == null){
+            return
+        }
+        val hidersNumberInput: EditText = findViewById(R.id.editHiders)
+        val hidersNumber: String = hidersNumberInput.text.toString().trim()
+
+        val seekersNumberInput: EditText = findViewById(R.id.editSeekers)
+        val seekersNumber: String = seekersNumberInput.text.toString().trim()
+
+
+
+        val gameTimeInput: EditText = findViewById(R.id.editGameTime)
+        val gameTime: String = gameTimeInput.text.toString().trim()
+
+
+        val gameSessionRef = database.getReference("gameSessions").push()
+
+        val players = listOf(
+            PlayerClass(username, true, null, null, true, true)
+        )
+
+        val sessionId: Int = Random().nextInt(9999 - 1000 + 1) + 1000
+        val sessionIdString: String = sessionId.toString()
+        val newGameSession = GameSessionClass(sessionIdString, "ongoing", players, 300, seekersNumber.toInt() ,hidersNumber.toInt())
+        gameSessionRef.setValue(newGameSession)
     }
 }
