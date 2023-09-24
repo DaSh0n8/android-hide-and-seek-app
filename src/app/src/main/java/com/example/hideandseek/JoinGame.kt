a9c7e8bf3344ab57004d5dc415bae7803faf8912
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2023-09-24 15:55:15 +1000

    feature: Merging of geofence configuration and new game setting completed

--- src/app/src/main/java/com/example/hideandseek/JoinGame.kt
+++ src/app/src/main/java/com/example/hideandseek/JoinGame.kt
@@ -1,11 +1,10 @@
 package com.example.hideandseek
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
