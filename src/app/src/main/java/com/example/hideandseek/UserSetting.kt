89ef9fd7f45d04834c44e17dc9697d3b9a7c0093
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2023-10-06 16:33:38 +1100

    fix: Reconstruction of app flow completed

--- src/app/src/main/java/com/example/hideandseek/UserSetting.kt
+++ src/app/src/main/java/com/example/hideandseek/UserSetting.kt
@@ -11,22 +11,32 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import java.io.ByteArrayOutputStream
 
 class UserSetting : AppCompatActivity() {
     /**
-     *  1) Get Lobby Code
-     *  2) Recursive passing of host value (to selfie segmentation and back)
+     *  1) Get Lobby Code DONE
+     *  2) Recursive passing of host value (to selfie segmentation and back) DONE
      *  3) Check unique username
      *  4) Add user to the game session in db
      */
     private var host: Boolean = false
     private var lobbyCode: String? = null
     private var userIcon: Bitmap? = null
+    private lateinit var database: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_setting)
+
+        FirebaseApp.initializeApp(this)
+        val databaseUrl = "https://db-demo-26f0a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        database = FirebaseDatabase.getInstance(databaseUrl)
 
         // receive the info and user icon if any value sent from previous activity
         host = intent.getBooleanExtra("host", false)
@@ -50,22 +60,81 @@ class UserSetting : AppCompatActivity() {
         changeIcon.setOnClickListener{ startActivity(changeIconIntent) }
 
         val confirmBtn: Button = findViewById(R.id.confirmBtn)
-        confirmBtn.setOnClickListener { confirmUserDetails(userIcon) }
+        confirmBtn.setOnClickListener { if (host) { hostConfirm(userIcon) } else { userConfirm(userIcon) }}
 
     }
 
-    private fun confirmUserDetails(userIcon: Bitmap?) {
+    private fun userConfirm(userIcon: Bitmap?) {
         val usernameInput: EditText = findViewById(R.id.username_input)
         val username: String = usernameInput.text.toString()
 
         if (username.isBlank()) {
             Toast.makeText(this@UserSetting, "Please enter a username", Toast.LENGTH_SHORT).show()
         } else {
-            val intent: Intent = if (host) {
-                Intent(this@UserSetting,NewGameSettings::class.java)
-            } else {
-                Intent(this@UserSetting,Lobby::class.java)
-            }
+            val query = database.getReference("gameSessions")
+                .orderByChild("sessionId")
+                .equalTo(lobbyCode)
+
+            query.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        val gameSessionSnapshot = dataSnapshot.children.first()
+                        val gameSession = gameSessionSnapshot.getValue(GameSessionClass::class.java)
+
+                        if (gameSession != null) {
+                            for (player in gameSession.players){
+                                if (username == player.userName) {
+                                    Toast.makeText(this@UserSetting, "Username already taken in game", Toast.LENGTH_SHORT).show()
+                                    return
+                                }
+                            }
+                            val newPlayer = PlayerClass(username, false, 0.0, 0.0, false, false)
+                            val updatedPlayers = gameSession.players.toMutableList()
+                            updatedPlayers.add(newPlayer)
+
+
+                            gameSession.players = updatedPlayers
+                            gameSessionSnapshot.ref.setValue(gameSession).addOnSuccessListener {
+                                val intent = Intent(this@UserSetting, Lobby::class.java)
+                                intent.putExtra("lobby_key", lobbyCode)
+
+                                if (userIcon != null) {
+                                    // compress the bitmap
+                                    val stream = ByteArrayOutputStream()
+                                    userIcon?.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                                    val byteArray = stream.toByteArray()
+
+                                    // pass the bitmap to next activity
+                                    intent.putExtra("userIcon", byteArray)
+                                }
+
+                                startActivity(intent)
+                            }.addOnFailureListener {
+                                Toast.makeText(this@UserSetting, "Error joining game", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            Toast.makeText(this@UserSetting, "Unexpected Error", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(this@UserSetting, "Invalid Lobby Code", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Toast.makeText(this@UserSetting, "Error fetching data", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+        }
+    }
+    private fun hostConfirm(userIcon: Bitmap?) {
+        val usernameInput: EditText = findViewById(R.id.username_input)
+        val username: String = usernameInput.text.toString()
+
+        if (username.isBlank()) {
+            Toast.makeText(this@UserSetting, "Please enter a username", Toast.LENGTH_SHORT).show()
+        } else {
+            val intent: Intent = Intent(this@UserSetting,NewGameSettings::class.java)
 
             if (userIcon != null) {
                 // compress the bitmap
@@ -77,11 +146,7 @@ class UserSetting : AppCompatActivity() {
                 intent.putExtra("userIcon", byteArray)
             }
 
-            if (lobbyCode != null) {
-                intent.putExtra("lobby_key", lobbyCode)
-            }
-
-            intent.putExtra("usernameKey", username)
+            intent.putExtra("username_key", username)
             startActivity(intent)
         }
     }
