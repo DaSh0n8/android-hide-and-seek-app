79d36bd76548c235535785d056ce9ecac76c1934
Yao Xian Phang <80228647+yaoxianphang@users.noreply.github.com>
2023-10-07 15:40:41 +1100

    feature: Enabled storing of user icon in database

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.camera:camera-view:1.2.2")
     implementation("androidx.camera:camera-extensions:1.2.2")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     implementation("com.google.mlkit:segmentation-selfie:16.0.0-beta4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
