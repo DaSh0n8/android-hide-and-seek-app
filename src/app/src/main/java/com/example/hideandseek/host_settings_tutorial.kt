0bdb350e0fde0e4b184d146bb3864bca2ce13230
zimop <zimopeng12345@gmail.com>
2023-11-02 14:01:45 +1100

    new tutorial

---
+++ src/app/src/main/java/com/example/hideandseek/host_settings_tutorial.kt
@@ -0,0 +1,11 @@
+package com.example.hideandseek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class host_settings_tutorial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host_settings_tutorial)
+    }
+}
